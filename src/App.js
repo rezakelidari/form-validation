dcf9b885295b383a09c34f3e187e37f04f078967
Reza Alone <reza.phoenix.1382@gmail.com>
2021-11-19 18:50:13 +0330

    Adding User page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import User from "./pages/User";
 import NotFound from "./pages/NotFound";
+
 import { LoginProvider } from "./provider/LoginProvider";
 
 function App() {
@@ -10,8 +13,9 @@ function App() {
     <LoginProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/user" element={<User />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
